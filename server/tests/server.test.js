539bf0e1d922281790341620606751db0a3613e6
Apple <apple@Apples-iMac.local>
2018-11-27 21:24:00 +0500

    Todos Test cases

---
+++ server/tests/server.test.js
@@ -0,0 +1,62 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server.js');
+
+const {Todo} = require('./../models/todo.js');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('Post /todos' , () => {
+  it('should create a new todo' , (done) => {
+    var text = 'Text todo text';
+
+    request(app)
+    .post('/todos')
+    .send({
+      text: text
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err , res) => {
+      if (err) {
+        return done(err)
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((e) => {
+        done(e);
+      });
+
+    });
+
+  });
+
+  it('should not add a todo with wrong body data' , (done) => {
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err , res) => {
+      if (err) {
+        return done(err)
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e) => {
+        done(e);
+      });
+
+    });
+  });
+
+});
